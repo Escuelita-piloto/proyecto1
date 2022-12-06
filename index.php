70f71449f32a68738446e084f3bd0a4b912e557b
El Richo <ricgarridoc@hotmail.com>
2022-12-05 23:56:41 -0600

    I modified again using VIM AGAIN

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 echo "Hola mundo, usando el editor VIM";
-echo "Este parece ser el flujo de trabajo de VIM: 1):edit nombreArchivo 2)presionar 2 veces la letra 'i' para empezar a escribir 3)";
-
+echo "Este parece ser el flujo de trabajo de VIM: 1):edit nombreArchivo 2)presionar 2 veces la letra 'i' para empezar a escribir 3)(ctrl+c) para que te deje escribir comando de VIM 4):wq par guardar y salir";
+echo "";
 ?>
 
